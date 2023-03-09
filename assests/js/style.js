66d595d7b5a88fcba3290697a3c14f5bad0c4c2b
NATE2043 <ncode2043@gmail.com>
2023-03-09 22:23:23 +0000

    add new game rock,paper,scissors first javaScript proect with css

---
+++ assests/js/style.js
@@ -0,0 +1,67 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const ChallangerScoreSpan = document.querySelector('[data-Challanger-score]')
+MeScoreSpan = document.querySelector('[data-Me-score]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '⛰',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '🧻',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂',
+        beats: 'paper'
+    }
+]
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+       const selectionName = selectionButton.dataset.selection
+       const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const ChallangerSelection = randomSelection()
+    const MeWinner = isWinner(selection, ChallangerSelection)
+    const ChallangerWinner = isWinner(ChallangerSelection, selection)
+
+    addSelectionResult(ChallangerSelection, ChallangerWinner)
+    addSelectionResult(selection, MeWinner)
+
+    if (MeWinner)incrementScore(MeScoreSpan)
+    if (ChallangerWinner) incrementScore(ChallangerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS [randomIndex]
+}
